664cb6387e41d03e99abc9569e4e66a7e1ac4384
fhtsX <60980602+fhtsX@users.noreply.github.com>
2020-04-16 12:09:19 -0300

    test_att2

--- Django_app/app_back/app_back/urls.py
+++ Django_app/app_back/app_back/urls.py
@@ -1,7 +1,7 @@
-"""TestProject URL Configuration
+"""app_back URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hello, index, saveToDb
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<name>', hello),
-    path('savetodb/<name>', saveToDb),
-    path('', index)
 ]
