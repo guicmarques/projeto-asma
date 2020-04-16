5d90b653288b1cf94502c3f08279a3c7dd41ce66
fhtsX <60980602+fhtsX@users.noreply.github.com>
2020-04-16 16:04:25 -0300

    Subida de adição de host de teste

--- Django_app/system/system/settings.py
+++ Django_app/system/system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@4*+qy^*b0k^8ft9rs&lyz4d7tip2dd^awzok-t$1x-8(s+&ui'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.225.179.134']
+ALLOWED_HOSTS = ['3.225.179.134','felipesibuya.me'] #Teste
 
 
 # Application definition
