d6d89dd044b4b89c234fe1dc33b51e20a984e82b
fhtsX <60980602+fhtsX@users.noreply.github.com>
2020-04-16 14:16:37 -0300

    test3

--- Django_app/system/system/urls.py
+++ Django_app/system/system/urls.py
@@ -1,4 +1,4 @@
-"""app_back URL Configuration
+"""system URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
