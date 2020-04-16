7a45c573c7801c4416ea0c2075df7c37c779ee61
fhtsX <60980602+fhtsX@users.noreply.github.com>
2020-04-16 12:21:59 -0300

    Update manage.py

--- Django_app/app_back/manage.py
+++ Django_app/app_back/manage.py
@@ -8,12 +8,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_back.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except:
+        pass
     execute_from_command_line(sys.argv)
 
 
