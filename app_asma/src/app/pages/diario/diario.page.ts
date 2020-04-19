3124d8ad62ddc5231babeae107fe428749401671
carolinamari <43223713+carolinamari@users.noreply.github.com>
2020-04-19 12:43:54 -0300

    page under construction with animation

--- app_asma/src/app/pages/diario/diario.page.ts
+++ app_asma/src/app/pages/diario/diario.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-diario',
@@ -6,11 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diario.page.scss'],
 })
 export class DiarioPage implements OnInit {
+  @ViewChild('dot1', {static: false}) dot1: ElementRef;
+  @ViewChild('dot2', {static: false}) dot2: ElementRef;
+  @ViewChild('dot3', {static: false}) dot3: ElementRef;
   imgBuilding: String = '../../../assets/images/em_construcao.svg';
+  dotA: Animation;
+  dotB: Animation;
+  dotC: Animation;
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.dotA = this.animationCtrl.create()
+      .addElement(this.dot1.nativeElement)
+      .fill('none')
+      .duration(2000)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'translateY(0px)' },
+        { offset: 0.125, transform: 'translateY(-5px)' },
+        { offset: 0.25, transform: 'translateY(0px)' },
+        { offset: 0.375, transform: 'translateY(0px)' },
+        { offset: 0.5, transform: 'translateY(0px)' },
+        { offset: 0.625, transform: 'translateY(0px)' },
+        { offset: 0.75, transform: 'translateY(0px)' },
+        { offset: 0.875, transform: 'translateY(0px)' },
+        { offset: 1, transform: 'translateY(0px)' }
+      ]);
+
+      this.dotB = this.animationCtrl.create()
+      .addElement(this.dot2.nativeElement)
+      .fill('none')
+      .duration(2000)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'translateY(0px)' },
+        { offset: 0.125, transform: 'translateY(0px)' },
+        { offset: 0.25, transform: 'translateY(0px)' },
+        { offset: 0.375, transform: 'translateY(-5px)' },
+        { offset: 0.5, transform: 'translateY(0px)' },
+        { offset: 0.625, transform: 'translateY(0px)' },
+        { offset: 0.75, transform: 'translateY(0px)' },
+        { offset: 0.875, transform: 'translateY(0px)' },
+        { offset: 1, transform: 'translateY(0px)' }
+      ]);
+
+      this.dotC = this.animationCtrl.create()
+      .addElement(this.dot3.nativeElement)
+      .fill('none')
+      .duration(2000)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'translateY(0px)' },
+        { offset: 0.125, transform: 'translateY(0px)' },
+        { offset: 0.25, transform: 'translateY(0px)' },
+        { offset: 0.375, transform: 'translateY(0px)' },
+        { offset: 0.5, transform: 'translateY(0px)' },
+        { offset: 0.625, transform: 'translateY(-5px)' },
+        { offset: 0.75, transform: 'translateY(0px)' },
+        { offset: 0.875, transform: 'translateY(0px)' },
+        { offset: 1, transform: 'translateY(0px)' }
+      ]);
+
+    this.dotA.play();
+    this.dotB.play();
+    this.dotC.play();
   }
 
 }
