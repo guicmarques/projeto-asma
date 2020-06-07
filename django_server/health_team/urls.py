45eaa651f6fc29e83edf914820d0e66c9f52b8db
fhts <60980602+fhts@users.noreply.github.com>
2020-06-07 17:45:56 -0300

    Update urls.py

--- django_server/health_team/urls.py
+++ django_server/health_team/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     url(r'^blank-1.html$', views.cadastroPaciente, name='cadastroPaciente'),
     url(r'^pacienteGraficos.html$', views.pacienteGraficos, name='pacienteGraficos'),
     url(r'^table_test.html$', views.tableTest, name='table_test'),
-    path('graphs/<id>', views.tableTest2, name='table_test2'),
+    path('graphs/<id>', views.pacienteGraficos, name='table_test2'),
 
 ]
