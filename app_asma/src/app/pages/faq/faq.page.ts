c9ea68a8ea33ed478db2d0331cb8630e99a02d1b
GabrielTakayanagi <58696611+GabrielTakayanagi@gmail.com>
2020-06-11 23:16:47 -0300

    Começo do FAQ

--- app_asma/src/app/pages/faq/faq.page.ts
+++ app_asma/src/app/pages/faq/faq.page.ts
@@ -8,74 +8,15 @@ import { Animation, AnimationController } from '@ionic/angular';
 })
 
 export class FaqPage implements OnInit {
-  @ViewChild('dot1', {static: false}) dot1: ElementRef;
-  @ViewChild('dot2', {static: false}) dot2: ElementRef;
-  @ViewChild('dot3', {static: false}) dot3: ElementRef;
-  imgBuilding: String = '../../../assets/images/em_construcao.svg';
-  dotA: Animation;
-  dotB: Animation;
-  dotC: Animation;
+  expandQuestions: boolean[] = [false,false,false,false];
 
   constructor(private animationCtrl: AnimationController) { }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-    this.dotA = this.animationCtrl.create()
-      .addElement(this.dot1.nativeElement)
-      .fill('none')
-      .duration(2000)
-      .iterations(Infinity)
-      .keyframes([
-        { offset: 0, transform: 'translateY(0px)' },
-        { offset: 0.125, transform: 'translateY(-5px)' },
-        { offset: 0.25, transform: 'translateY(0px)' },
-        { offset: 0.375, transform: 'translateY(0px)' },
-        { offset: 0.5, transform: 'translateY(0px)' },
-        { offset: 0.625, transform: 'translateY(0px)' },
-        { offset: 0.75, transform: 'translateY(0px)' },
-        { offset: 0.875, transform: 'translateY(0px)' },
-        { offset: 1, transform: 'translateY(0px)' }
-      ]);
-
-      this.dotB = this.animationCtrl.create()
-      .addElement(this.dot2.nativeElement)
-      .fill('none')
-      .duration(2000)
-      .iterations(Infinity)
-      .keyframes([
-        { offset: 0, transform: 'translateY(0px)' },
-        { offset: 0.125, transform: 'translateY(0px)' },
-        { offset: 0.25, transform: 'translateY(0px)' },
-        { offset: 0.375, transform: 'translateY(-5px)' },
-        { offset: 0.5, transform: 'translateY(0px)' },
-        { offset: 0.625, transform: 'translateY(0px)' },
-        { offset: 0.75, transform: 'translateY(0px)' },
-        { offset: 0.875, transform: 'translateY(0px)' },
-        { offset: 1, transform: 'translateY(0px)' }
-      ]);
-
-      this.dotC = this.animationCtrl.create()
-      .addElement(this.dot3.nativeElement)
-      .fill('none')
-      .duration(2000)
-      .iterations(Infinity)
-      .keyframes([
-        { offset: 0, transform: 'translateY(0px)' },
-        { offset: 0.125, transform: 'translateY(0px)' },
-        { offset: 0.25, transform: 'translateY(0px)' },
-        { offset: 0.375, transform: 'translateY(0px)' },
-        { offset: 0.5, transform: 'translateY(0px)' },
-        { offset: 0.625, transform: 'translateY(-5px)' },
-        { offset: 0.75, transform: 'translateY(0px)' },
-        { offset: 0.875, transform: 'translateY(0px)' },
-        { offset: 1, transform: 'translateY(0px)' }
-      ]);
-
-    this.dotA.play();
-    this.dotB.play();
-    this.dotC.play();
+  };
+  expand(questionNumber){
+   
+    this.expandQuestions[questionNumber] = !(this.expandQuestions[questionNumber]);
   }
 
 }
