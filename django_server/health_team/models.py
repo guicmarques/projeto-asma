588edb21ebc6f26646b17792a2408266953d5eb8
fhts <60980602+fhts@users.noreply.github.com>
2020-06-15 22:31:27 -0300

    Update models.py

--- django_server/health_team/models.py
+++ django_server/health_team/models.py
@@ -1,1 +1,1 @@
-from server.models import User, UserProfileInfo, AsthmaControlQuestionnaire, FitbitFile, DailyControl
+from server.models import User, UserProfileInfo, AsthmaControlQuestionnaire, FitbitFile, DailyControl, PracticeBarriers
