5f76eec07913d80a0f7c26996aeaef44d514148b
fhts <60980602+fhts@users.noreply.github.com>
2020-06-20 11:50:55 -0300

    Update views.py

--- django_server/health_team/views.py
+++ django_server/health_team/views.py
@@ -740,7 +740,7 @@ def pacienteGraficos2(request,username):
         fig['layout']['sliders'][0]['steps'][i]['label']=dates[i]
 
     fig['layout'].update(
-        height=400
+        height=600
     )
     barreiras = plot({"data":fig},output_type='div', include_plotlyjs=True, show_link=False, link_text="", auto_open=False)
 
