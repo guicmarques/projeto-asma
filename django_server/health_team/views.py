1a6e18ad26da5f1a61a16720f82acc9ac6a9ddef
fhts <60980602+fhts@users.noreply.github.com>
2020-06-21 00:22:48 -0300

    Update views.py

--- django_server/health_team/views.py
+++ django_server/health_team/views.py
@@ -1287,13 +1287,13 @@ def downloadFitBitData(request):
 
     for user_id_ind in usuariosFitBit:
         try:
-            print(user_id_ind,User.objects.get(id=user_id_ind))
-            dados60dias = getFitbitData(user=User.objects.get(id=user_id_ind),dates=day60List)
+            print(user_id_ind,User.objects.get(userId=user_id_ind))
+            dados60dias = getFitbitData(user=User.objects.get(userId=user_id_ind),dates=day60List)
             for day in sorted(dados60dias.keys(),reverse=True):
                 writer.writerow(user_id_ind, day, dados60dias[day]["summary"]["steps"], dados60dias[day]["summary"]["sedentaryMinutes"], dados60dias[day]["summary"]["lightlyActiveMinutes"], dados60dias[day]["summary"]["veryActiveMinutes"])
         except Exception:
             pass
-            traceback.print_exc()
+            #traceback.print_exc()
 
     print(usuariosFitBit)
     response['Content-Disposition'] = 'attachment; filename="AsthmaControlQuestionnaire.csv"'
